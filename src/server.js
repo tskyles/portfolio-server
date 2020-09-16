e28d6822323bfaddbf6b76656b49ab88cd79a6fa
travis skyles <travis.skyles@gmail.com>
2020-09-15 18:24:35 -0700

    server up and sending mail

---
+++ src/server.js
@@ -0,0 +1,58 @@
+'use strict';
+
+require('dotenv').config();
+const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const mailer = require('nodemailer');
+
+const server = express();
+
+server.use(cors());
+server.use(bodyParser.urlencoded({ extended: true }));
+server.use(bodyParser.json());
+
+server.post('/sendmail', (req, res) => {
+	console.log('here');
+	console.log(req.body);
+	const { name, email, subject, message } = req.body;
+	const transporter = mailer.createTransport({
+		service: 'SendinBlue',
+		auth: {
+			user: process.env.TRANSPORTER_USER,
+			pass: process.env.TRANSPORTER_PASSWORD,
+		},
+	});
+
+	const mailOptions = {
+		from: email,
+		to: process.env.RECIEVER_EMAIL,
+		subject: `From ${name}: ${subject}`,
+		text: message,
+	};
+
+	// verify connection configuration
+	transporter.verify(function (error, success) {
+		if (error) {
+			console.log(error);
+		} else {
+			console.log('Server is ready to take our messages');
+		}
+	});
+
+	transporter.sendMail(mailOptions, (error, info) => {
+		if (error) {
+			console.log(error);
+			res.status(500).send('Error sending message');
+		} else {
+			console.log(info);
+			res.status(200).send('Message sent successfully');
+		}
+	});
+});
+
+module.exports = {
+	server: server,
+	start: (PORT) =>
+		server.listen(PORT, () => console.log(`server up on ${PORT}`)),
+};
